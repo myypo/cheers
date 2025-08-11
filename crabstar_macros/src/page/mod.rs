e1422c863792623d5c8b24e4f5092e2340daad84
myypo <nikirsmcgl@gmail.com>
2025-08-12 00:48:05 +0300

    feat: remove dependency of generated suspense code on tokio-stream

--- crabstar_macros/src/page/mod.rs
+++ crabstar_macros/src/page/mod.rs
@@ -20,6 +20,22 @@ fn into_response_impl(p: &Result<fragment::Params, Error>) -> TokenStream {
             (::axum::http::StatusCode::OK, ::axum::response::Html(body)).into_response()
         }
     } else {
+        let stream_impl = quote! {
+            struct UnboundedReceiverStream<T>(::tokio::sync::mpsc::UnboundedReceiver<T>);
+            impl<T> ::futures::stream::Stream for UnboundedReceiverStream<T> {
+                type Item = T;
+
+                fn poll_next(
+                        mut self: ::std::pin::Pin<&mut Self>,
+                        cx: &mut ::std::task::Context<'_>,
+                    ) -> ::std::task::Poll<::std::option::Option<Self::Item>> {
+                    self.0.poll_recv(cx)
+                }
+            }
+
+            UnboundedReceiverStream(rx)
+        };
+
         quote! {
             use ::askama::Template;
             use ::axum::response::IntoResponse;
@@ -34,7 +50,7 @@ fn into_response_impl(p: &Result<fragment::Params, Error>) -> TokenStream {
                     }
                 });
 
-                ::tokio_stream::wrappers::UnboundedReceiverStream::new(rx)
+                #stream_impl
             };
             let body = ::axum::body::Body::from_stream(body);
 
