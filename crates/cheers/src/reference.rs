de5df24a5790d680b42d2c15aec0e2ca29b266a4
myypo <nikirsmcgl@gmail.com>
2026-03-01 21:44:51 +0200

    feat: implement Deserialize, PartialEq and PartialOrd for ElementId

--- crates/cheers/src/reference.rs
+++ crates/cheers/src/reference.rs
@@ -1,19 +1,31 @@
 use std::{fmt::Display, marker::PhantomData};
 
+use serde::Deserialize;
+
 use crate::{
     context::AttributeValue,
     render::{Buffer, Render},
 };
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub(crate) enum InnerElementId {
     Static(&'static str),
     Dynamic(String),
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 #[repr(transparent)]
 pub struct ElementId(pub(crate) InnerElementId);
+
+impl<'de> Deserialize<'de> for ElementId {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Ok(ElementId(InnerElementId::Dynamic(s)))
+    }
+}
 
 impl Display for ElementId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
